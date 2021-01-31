cfc19e77bb35e74c5dafd1ae779ac04a55ca5307
sjohnston82 <72237516+sjohnston82@users.noreply.github.com>
2021-01-31 00:58:40 -0800

    navbar and page components

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom'
 
+import Navs from "./components/Navs"
+import Home from "./pages/Home"
+import Starred from "./pages/Starred"
+
 function App() {
   return (
-    <Switch>
-      <Route exact path="/">
-        This is home page
-      </Route>
+    <div>
+      <Navs />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
 
-      <Route exact path="/starred">
-        This is starred
-      </Route>
+        <Route exact path="/starred">
+          <Starred />
+        </Route>
 
-      <Route>404</Route>
-    </Switch>
+        <Route>
+          <div>
+            Page not found: 404
+          </div>
+        </Route>
+      </Switch>
+    </div>
   );
 }
 
