abc70e65cc25d8308090c69cebc49a4ec6a580ee
sjohnston82 <72237516+sjohnston82@users.noreply.github.com>
2021-02-03 14:39:30 -0800

    styling components

--- src/components/show/ShowGrid.js
+++ src/components/show/ShowGrid.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import ShowCard from './ShowCard'
 
+import { FlexGrid } from '../styled'
+
 import IMAGE_NOT_FOUND from '../../images/not-found.png'
 
 const ShowGrid = ({ data }) => {
   return (
-    <div>
+    <FlexGrid>
       { 
         data.map( ({ show })=> 
         <ShowCard 
@@ -16,7 +18,7 @@ const ShowGrid = ({ data }) => {
           summary={show.summary}
         />)
       }
-    </div>
+    </FlexGrid>
   )
 }
 
